4a7784375cce1638f2bb4db2980852e878edb353
themother <jacktowleretc@hotmail.com>
2023-12-01 12:26:47 +0000

    real day 1 done

--- aoc/advent/static/advent/js/day_1.js
+++ aoc/advent/static/advent/js/day_1.js
@@ -1,30 +1,78 @@
-function part1() {
-    var data = document.getElementById("input").value.split('');
+const ns = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
 
-    let i = 0, tot = 0;
-    while (i < data.length - 1) {
-        if (data[i] == data[i + 1]) {
-            tot += Number(data[i])
+function checkChar(c) {
+    return c >= '0' && c <= '9'
+}
+
+function getFirst(chars) {
+    for (c of chars) {
+        if (checkChar(c)) {
+            return c;
+        }
+    }
+}
+
+function getLast(chars) {
+    var i = chars.length - 1;
+    while (!checkChar(chars[i])) {
+        i--;
+    }
+    return chars[i];
+}
+
+function replaceLastString(line) {
+    var i = 0, index = -1, nsValue = "";
+    while (i < line.length) {
+        for (const n of ns) {
+            if (line.substring(i, i + n.length) == n) {
+                index = i;
+                nsValue = n;
+            }
         }
         i++;
     }
-
-    if (data[0] == data[data.length - 1]) {
-        tot += Number(data[0])
+    if (index >= 0) {
+        var rString = (ns.indexOf(nsValue) + 1).toString();
+        return line.substring(0, index) + rString + line.substring(index + nsValue.length, line.length);
     }
+    return line;
+}
 
+
+function replaceFirstString(line) {
+    let i = 0;
+    while (i < line.length) {
+        for (const n of ns) {
+            if (line.substring(i, i + n.length) == n) {
+                var newString = (ns.indexOf(n) + 1).toString();
+                return line.replace(n, newString);
+            }
+        }
+        i++;
+    }
+    return line;
+}
+
+function part1() {
+    var data = document.getElementById("input").value.split('\n');
+    var tot = 0;
+    for (line of data) {
+        var chars = line.split('');
+        var first = getFirst(chars);
+        var last = getLast(chars);
+        tot += parseInt(first + last);
+    }
     document.getElementById("part-1-answer").textContent = tot;
 }
 
 function part2() {
-    var data = document.getElementById("input").value.split('');
-    var l = data.length / 2;
-    let i = 0, tot = 0;
-    while (i < l) {
-        if (data[i] == data[i + l]) {
-            tot += Number(data[i])
-        }
-        i++;
+    var rawData = document.getElementById("input").value.split('\n');
+    var t = 0;
+    for (var line of rawData) {
+        var first = getFirst(replaceFirstString(line).split(''));
+        var last = getLast(replaceLastString(line).split(''));
+        t += parseInt(first + last);
     }
-    document.getElementById("part-2-answer").textContent = tot * 2;
+
+    document.getElementById("part-2-answer").textContent = t;
 }

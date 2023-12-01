8a0323e547d6468c37c11f7a14a39e3c992a2bf4
themother <jacktowleretc@hotmail.com>
2023-12-01 20:53:56 +0000

    string representation for models

--- aoc/advent/models.py
+++ aoc/advent/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Day(models.Model):
     day_id = models.IntegerField(unique=True)
 
+    def __str__(self):
+        return self.day_id
+
 
 class Data(models.Model):
     day = models.ForeignKey(Day, on_delete=models.CASCADE)
@@ -11,3 +14,8 @@ class Data(models.Model):
     main_input = models.BooleanField()
     expected_output_1 = models.CharField(max_length=255, null=True, blank=True)
     expected_output_2 = models.CharField(max_length=255, null=True, blank=True)
+
+
+    def __str__(self):
+        prefix = 'Input' if self.main_input else 'E.g.'
+        return f'{prefix}: {self.input_data[:10]}'

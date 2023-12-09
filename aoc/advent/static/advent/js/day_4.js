92c7289d0520029f800109f2052b1fb6617e693d
themother <jacktowleretc@hotmail.com>
2023-12-09 11:44:09 +0000

    tidied day 4

--- aoc/advent/static/advent/js/day_4.js
+++ aoc/advent/static/advent/js/day_4.js
@@ -2,37 +2,13 @@ function countWins(line, updateT) {
   let split = line.split(" | ");
   let nums = split[0].split(" ");
   let wins = split[1].split(" ");
-  let t = 0;
-  for (n of nums) {
-    if (wins.includes(n) && n != "" && n!= " ") {
-      t = updateT(t);
-    }
-  }
-  return t;
+  return nums.filter((n) => wins.includes(n) && n != "" && n!= " ").reduce((a, c) => a + updateT(a), 0);
 }
-
-function incT(t) {
-  return t + 1
-}
-
-function doubleT(t) {
-  if (t == 0) {
-    return 1;
-  } else {
-    return t * 2;
-  }
-}
-
 
 function getCopies(cards, ks, vs) {
-  let r = [];
-  for (card of cards) {
-    let v = vs[ks.indexOf(card)];
-    for (let i = 1; i <= v; i++) {
-      r.push(card + i)
-    }
-  }
-  return r;
+  return = cards.flatMap(function(card) {
+    return [...Array(vs[ks.indexOf(card)])].map((x, i) => i + 1 + card);
+  })
 }
 
 function getNumber(l) {
@@ -42,28 +18,24 @@ function getNumber(l) {
 
 function part1() {
     let data = document.getElementById("input").value.split('\n');
-    let t = 0;
-    for (line of data) {
-      let x = countWins(line.split(": ")[1], doubleT);
-      t += x;
-    }
+    let t = data.reduce((a, c) => a + countWins(line.split(": ")[1], (t) => t == 0 ? 1 : t * 2))
     document.getElementById("part-1-answer").textContent = t;
 }
 
 function part2() {
-  var data = document.getElementById("input").value.split('\n');
+  let data = document.getElementById("input").value.split('\n');
   let ks = [], vs = [];
   for (line of data) {
     let split = line.split(": ");
     ks.push(getNumber(split[0]));
-    let x = countWins(split[1], incT);
+    let x = countWins(split[1], (t => t + 1));
     vs.push(x);
   }
   let t = ks.length;
   let newCards = getCopies(ks, ks, vs);
   while (newCards.length > 0) {
     t += newCards.length;
-    newCards = getCopies(newCards, ks, vs);
+    newCards = gC(newCards, ks, vs);
   }
   document.getElementById("part-2-answer").textContent = t;
 }

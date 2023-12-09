1f3390c289d45f3d26075b5e2ebfbf152de88104
themother <jacktowleretc@hotmail.com>
2023-12-09 10:52:32 +0000

    tidied day 7

--- aoc/advent/static/advent/js/day_7.js
+++ aoc/advent/static/advent/js/day_7.js
@@ -10,44 +10,35 @@ function eqArr(a1, a2) {
   return true;
 }
 
-function sortHands(h1, h2) {
+function sortHands(h1, h2, orderStr) {
   if (h1.rank == h2.rank) {
     for (let i = 0; i < h1.cards.length; i++) {
       if (h1.cards[i] != h2.cards[i]) {
-        if (order.indexOf(h1.cards[i]) < order.indexOf(h2.cards[i]) ) {
-          return 1;
-        } else {
-          return -1;
-        }
+        return orderStr.indexOf(h1.cards[i]) < orderStr.indexOf(h2.cards[i]) ? 1 : -1;
       }
     }
-
-  } else if (h1.rank < h2.rank){
-    return 1;
   } else {
-  return -1;
+    return h1.rank < h2.rank ? 1 : -1;
   }
 }
+
+
+function sortHands1(h1, h2) {
+  return sortHands(h1, h2, order)
+}
+
 function sortHands2(h1, h2) {
-  if (h1.rank == h2.rank) {
-    for (let i = 0; i < h1.cards.length; i++) {
-      if (h1.cards[i] != h2.cards[i]) {
-        if (newOrder.indexOf(h1.cards[i]) < newOrder.indexOf(h2.cards[i]) ) {
-          return 1;
-        } else {
-          return -1;
-        }
-      }
-    }
-
-  } else if (h1.rank < h2.rank){
-    return 1;
-  } else {
-  return -1;
-  }
+  return sortHands(h1, h2, newOrder)
 }
 
-function getRank(cards) {
+function getHands(line, rankF) {
+  let cards = line.split(" ")[0];
+  let bid = Number(line.split(" ")[1]);
+  let rank = rankF(cards);
+  return {"cards": cards, "bid": bid, "rank": rank};
+}
+
+function getV(cards) {
   let ks = [], v = [];
   for (card of cards.split('')) {
     if (ks.includes(card)) {
@@ -57,7 +48,11 @@ function getRank(cards) {
       v.push(1)
     }
   }
-  v = v.sort();
+  return v.sort();
+}
+
+function getRank(cards) {
+  v = getV(cards);
   if(eqArr(v, [5])) {
     return 1;
   } else if (eqArr(v, [1, 4])) {
@@ -76,41 +71,16 @@ function getRank(cards) {
 }
 
 function getRank2(cards) {
-  let ks = [], v = [];
-  for (card of cards.split('')) {
-    if (ks.includes(card)) {
-      v[ks.indexOf(card)]++;
-    } else {
-      ks.push(card);
-      v.push(1)
-    }
-  }
-  let n = 0;
-  if (ks.includes('J')) {
-    n = v[ks.indexOf('J')];
-  }
-
-  v = v.sort();
+  let n = cards.split('').filter((x) => x == 'J').length;
+  v = getV(cards);
   if(eqArr(v, [5])) {
     return 1;
   } else if (eqArr(v, [1, 4])) {
-    if (n > 0) {
-      return 1;
-    } else {
-      return 2;
-    }
+    return n > 0 ? 1 : 2;
   } else if (eqArr(v, [2, 3])) {
-    if (n > 0) {
-      return 1;
-    } else {
-      return 3;
-    }
+    return n > 0 ? 1 : 3;
   } else if (eqArr(v, [1, 1, 3])) {
-    if (n > 0) {
-      return 2;
-    } else {
-      return 4;
-    }
+    return n > 0 ? 2 : 4;
   } else if (eqArr(v, [1, 2, 2])) {
     if (n == 1) {
       return 3;
@@ -120,50 +90,25 @@ function getRank2(cards) {
       return 5;
     }
   } else if (eqArr(v, [1, 1, 1, 2])) {
-    if (n > 0) {
-      return 4;
-    } else {
-      return 6;
-    }
+    return n > 0 ? 4 : 6;
   } else {
-    if (n > 0) {
-      return 6;
-    } else {
-      return 7;
-    }
+    return n > 0 ? 6 : 7;
   }
 }
 
 function part1() {
   let data = document.getElementById("input").value.split('\n');
-  let hands = [];
-
-  for (line of data) {
-    let cards = line.split(" ")[0];
-    let bid = Number(line.split(" ")[1]);
-    let rank = getRank(cards);
-    hands.push({"cards": cards, "bid": bid, "rank": rank})
-  }
-  hands = hands.sort(sortHands);
+  let hands = data.map((x) => getHands(x, getRank)).sort(sortHands1);
   let t = 0;
   for (let i = 0; i < hands.length; i++) {
     t += (i + 1) * hands[i].bid;
   }
-
   document.getElementById("part-1-answer").textContent = t;
 }
 
 function part2() {
-  var data = document.getElementById("input").value.split('\n');
-let hands = [];
-
-  for (line of data) {
-    let cards = line.split(" ")[0];
-    let bid = Number(line.split(" ")[1]);
-    let rank = getRank2(cards);
-    hands.push({"cards": cards, "bid": bid, "rank": rank})
-  }
-  hands = hands.sort(sortHands2);
+  let data = document.getElementById("input").value.split('\n');
+  let hands = data.map((x) => getHands(x, getRank2)).sort(sortHands2);
   let t = 0;
   for (let i = 0; i < hands.length; i++) {
     t += (i + 1) * hands[i].bid;

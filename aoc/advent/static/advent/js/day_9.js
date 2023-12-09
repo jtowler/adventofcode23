5e680e6fb8a344dc372ce7dfb0933a8c433dce2c
themother <jacktowleretc@hotmail.com>
2023-12-09 10:11:16 +0000

    tidied day 9

--- aoc/advent/static/advent/js/day_9.js
+++ aoc/advent/static/advent/js/day_9.js
@@ -1,53 +1,31 @@
 function pred(h) {
   let iters = [h];
-  while (getNUnique(h)) {
+  while (new Set(h).size > 1){
     h = h.slice(1).map((a, i) => a - h[i]);
     iters.push(h);
   }
   iters = iters.toReversed();
-
-  let start = iters[0][0];
-  for (it of iters.slice(1)) {
-    start = it[it.length - 1] + start;
-  }
-  return start;
-}
-
-function getNUnique(h) {
-  let s = new Set(h);
-  return s.size > 1
+  return iters.slice(1).reduce((a, c) => c[c.length - 1] + a, iters[0][0]);
 }
 
 function predNeg(h) {
   let iters = [h];
-  while (getNUnique(h)) {
+  while (new Set(h).size > 1) {
     h = h.slice(1).map((a, i) => a - h[i]);
     iters.push(h);
   }
   iters = iters.toReversed();
-  let start = iters[0][0];
-  for (it of iters.slice(1)) {
-    start = it[0] - start;
-  }
-  return start;
+  return iters.slice(1).reduce((a, c) => c[0] - a, iters[0][0]);
 }
 
 function part1() {
   let data = document.getElementById("input").value.split('\n');
-  let hists = data.map((line) => line.split(" ").map((x) => Number(x)));
-
-  let m = hists.map(pred);
-  let s = m.reduce((a,b) => a+b)
-
-  document.getElementById("part-1-answer").textContent = s;
+  let hists = data.map((line) => line.split(" ").map(Number));
+  document.getElementById("part-1-answer").textContent = hists.map(pred).reduce((a,b) => a+b);
 }
 
 function part2() {
   let data = document.getElementById("input").value.split('\n');
-  let hists = data.map((line) => line.split(" ").map((x) => Number(x)));
-
-  let m = hists.map(predNeg);
-  let s = m.reduce((a,b) => a+b)
-
-  document.getElementById("part-2-answer").textContent = s;
+  let hists = data.map((line) => line.split(" ").map(Number));
+  document.getElementById("part-2-answer").textContent = hists.map(predNeg).reduce((a,b) => a+b);
 }
